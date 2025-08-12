d58ec5d779c684f1d548e0e9889bd3dde0cff7b8
Manas Pande <manaspande54+github@gmail.com>
2025-08-13 00:02:42 +0530

    init : Figment 0.0.1

---
+++ Program.cs
@@ -0,0 +1,80 @@
+﻿// Usings
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+class Program
+{
+    static async Task Main()
+    {
+        string[] options = { "Browse directory for files", "Find duplicate files in directory" };
+        int selectedIndex = 0;
+
+        ConsoleKey key;
+        do
+        {
+            Console.Clear();
+            Console.WriteLine("Figment\n");
+            Console.WriteLine("Use ↑ and ↓ to navigate, Enter to select:\n");
+
+            for (int i = 0; i < options.Length; i++)
+            {
+                if (i == selectedIndex)
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine($"> {options[i]}");
+                    Console.ResetColor();
+                }
+                else
+                {
+                    Console.WriteLine($"  {options[i]}");
+                }
+            }
+
+            key = Console.ReadKey(true).Key;
+
+            if (key == ConsoleKey.UpArrow)
+                selectedIndex = (selectedIndex == 0) ? options.Length - 1 : selectedIndex - 1;
+            else if (key == ConsoleKey.DownArrow)
+                selectedIndex = (selectedIndex + 1) % options.Length;
+
+        } while (key != ConsoleKey.Enter);
+
+        Console.Clear();
+        switch (selectedIndex)
+        {
+            case 0:
+                Console.WriteLine("Browsing directory for files...");
+                await ShowSpinnerForDuration(3000);
+                Console.WriteLine("\nDirectory browsing complete.");
+                break;
+            case 1:
+                Console.WriteLine("Finding duplicates in files...");
+                await ShowSpinnerForDuration(5000);
+                Console.WriteLine("\nDuplicate search complete.");
+                break;
+        }
+    }
+
+    static async Task ShowSpinnerForDuration(int durationMs)
+    {
+        using var cts = new CancellationTokenSource();
+        var spinnerTask = ShowSpinner(cts.Token);
+        await Task.Delay(durationMs);
+        cts.Cancel();
+        await spinnerTask;
+    }
+
+    static async Task ShowSpinner(CancellationToken token)
+    {
+        var sequence = new[] { '|', '/', '-', '\\' };
+        int counter = 0;
+        while (!token.IsCancellationRequested)
+        {
+            Console.Write(sequence[counter % sequence.Length]);
+            Console.Write('\b');
+            counter++;
+            await Task.Delay(100);
+        }
+    }
+}
